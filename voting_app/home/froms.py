a04762dd14cf4ab400b4f2aad13e15cf994ed3e7
Neeraj <itskop520@gmail.com>
2020-11-18 22:10:58 +0545

    delted bich

--- voting_app/home/froms.py
+++
@@ -1,5 +0,0 @@
-# from .models import Question , Choice
-# from django import forms
-# from django.shortcuts import render , HttpResponse , redirect
-# def voting(reqest):
-#     return render(reqest , 'create_vote.html')
--- voting_app/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path , include
-from home import urls , views
-from home.views import view_qestion , vote_here
-app_name = 'home'
-urlpatterns = [
-    path('',  view_qestion.as_view(), name = 'home'),
-    path('vote_here/<int:pk_second>', views.vote_here , name = 'vote_here'),
-    path('new' , views.new_vote , name = 'new')
-]
